ba616c3626efdab9610fa00da9c036bc2950aa5a
Niony <nionyandria@gmail.com>
2017-05-18 00:31:00 +0200

    ArticleFacadeLocal GetsionArtilce + Local ServiceCo + Local Clien lourd

--- PrestaZon_HAN/PrestaZon_HAN-ejb/src/java/controllers/ArticleFacade.java
+++ PrestaZon_HAN/PrestaZon_HAN-ejb/src/java/controllers/ArticleFacade.java
@@ -15,9 +15,10 @@ import javax.persistence.PersistenceContext;
  * @author Alexis
  */
 @Stateless
-public class ArticleFacade extends AbstractFacade<Article> {
+public class ArticleFacade extends AbstractFacade<Article>
+    implements ArticleFacadeLocal {
 
-    @PersistenceContext(unitName = "PrestaZon_HAN-ejbPU")
+    @PersistenceContext (unitName = "PrestaZon_HAN-ejbPU")
     private EntityManager em;
 
     @Override
@@ -28,5 +29,32 @@ public class ArticleFacade extends AbstractFacade<Article> {
     public ArticleFacade() {
         super(Article.class);
     }
+    /*Ajouter article : requete qui va chercher dans la base*/
+    //Voir ClientFacade TP Banque => find dans la diagramme de sequence => chercherClient a partir de son nom prenom pour avoir ID
+    //Gestion Client appele la fonction chercherClient dans Facade
+    //public Integer ajouterArticle(Integer reference, String type,Double prixUnitaire){
+        //requette ajout article avec MySQL //ajout dans classe Article comme Client 
+        // return null;
+    //}
     
+    /*Retirer article : requete qui va chercher dans la base*/
+   // public void retirerArticle(Integer reference){
+        //requette ajout article avec MySQL
+         
+    //}
+    /*Afficher article : requete qui va chercher dans la base*/
+    //public void afficherArticle(Integer reference){
+        //requette ajout article avec MySQL
+         
+    //}
+
+ 
+    @Override
+    public void creerArticle(String type,Double prixUnitaire){
+        Article art = new Article();
+        art.setType(type);
+        art.setPrixUnitaire(prixUnitaire);
+        em.persist(art);
+    }
+
 }
